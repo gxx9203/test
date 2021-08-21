fb430545da75e51e42cb4904b34ea8cda8469d2d
gxx9203 <gxx9203@163.com>
2021-08-21 17:20:40 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 echo `date`
 echo `pwd`
 repo init --repo-url=https://mirrors.tuna.tsinghua.edu.cn/git/git-repo --no-repo-verify --repo-branch=stable -u https://github.com/gxx9203/manifests.git -b default -m hexo.xml
-
+ls -la
