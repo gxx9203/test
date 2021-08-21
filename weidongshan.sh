0be5d246620d79ff6a861b81c474ec3502e570a7
gxx9203 <gxx9203@163.com>
2021-08-21 18:48:55 +0800

    Update weidongshan.sh

--- weidongshan.sh
+++ weidongshan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir weidognshan
 cd weidognshan
-repo init --repo-url=https://mirrors.tuna.tsinghua.edu.cn/git/git-repo --no-repo-verify --repo-branch=stable -u git@github.com:gxx9203/manifests.git -m weidongshang.xml
+repo init --repo-url=https://mirrors.tuna.tsinghua.edu.cn/git/git-repo --no-repo-verify --repo-branch=stable -u git@github.com:gxx9203/manifests.git -b default -m weidongshang.xml
 repo sync 
 
 ls -la
